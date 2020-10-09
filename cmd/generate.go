f94a76c65d5e31d4531bbd6d21679abe6eb32e0b
Oliver Braun <ob@cs.hm.edu>
2020-10-09 17:15:39 +0200

    remove unused variable

--- cmd/generate.go
+++ cmd/generate.go
@@ -5,21 +5,15 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var (
-	Startercode string
-)
-
 func init() {
 	rootCmd.AddCommand(generateCmd)
-	generateCmd.Flags().StringVarP(&Startercode, "startercode", "c", "",
-		"repo which should be used as starter code template")
 }
 
 var generateCmd = &cobra.Command{
 	Use:   "generate [group] [assignment]",
 	Short: "Generate repositories for each student.",
 	Long:  `Generate repositories for each student in [group] for [assignment]`,
-	Args:  cobra.ExactArgs(2),
+	Args:  cobra.ExactArgs(2), //nolint:gomnd
 	Run: func(cmd *cobra.Command, args []string) {
 		c := gitlab.NewClient()
 		c.Generate(args[0], args[1], "")

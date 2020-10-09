f5f5355030579eabdfa4f0cba1dbc9a5328aeb97
Oliver Braun <ob@cs.hm.edu>
2020-10-09 17:22:12 +0200

    remove show-info command

--- gitlab/gitlab.go
+++ gitlab/gitlab.go
@@ -1,9 +1,6 @@
 package gitlab
 
 import (
-	"encoding/json"
-	"fmt"
-
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/viper"
 	"github.com/xanzy/go-gitlab"
@@ -24,15 +21,4 @@ func NewClient() *Client {
 	}
 
 	return &Client{client}
-}
-
-func (c *Client) GetGroupInfo(groupname string) {
-	group, resp, err := c.Groups.GetGroup(viper.GetInt(groupname + ".id"))
-	if err != nil {
-		log.Error().Err(err).Str("group", groupname).Msg("error while fetching group info")
-	}
-
-	groupInfo, _ := json.MarshalIndent(group, "", "  ")
-	fmt.Printf("%s\n", groupInfo)
-	fmt.Printf("%+v\n", resp.Response.Status)
 }

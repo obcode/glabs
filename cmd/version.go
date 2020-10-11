c6f677d6ae82a6f79ef3edf9db9992a7a5abca9b
Oliver Braun <ob@cs.hm.edu>
2020-10-11 13:39:03 +0200

    inject version et al by goreleaser

--- cmd/version.go
+++ cmd/version.go
@@ -4,9 +4,8 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
-
-const version = "0.0"
 
 func init() {
 	rootCmd.AddCommand(versionCmd)
@@ -17,6 +16,11 @@ var versionCmd = &cobra.Command{
 	Short: "Print the version number of Glabs",
 	Long:  `All software has versions. This is Glabs'`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Glabs version %s\n", version)
+		fmt.Printf("Glabs version %s, commit %s, build date %s, build by %s\n",
+			viper.GetString("Version"),
+			viper.GetString("Commit"),
+			viper.GetString("Date"),
+			viper.GetString("BuiltBy"),
+		)
 	},
 }

8e6a726b3d596bb94b94ee00dee8f8621986e9fb
Oliver Braun <ob@cs.hm.edu>
2024-11-05 12:14:10 +0100

    feat(urls): urls without groups or students prints the assignment url

--- config/urls.go
+++ config/urls.go
@@ -2,8 +2,10 @@ package config
 
 import "fmt"
 
-func (cfg *AssignmentConfig) Urls() {
-	if cfg.Per == PerStudent {
+func (cfg *AssignmentConfig) Urls(assignment bool) {
+	if assignment {
+		fmt.Println(cfg.URL)
+	} else if cfg.Per == PerStudent {
 		for _, stud := range cfg.Students {
 			fmt.Printf("%s/%s-%s\n", cfg.URL, cfg.Name, cfg.RepoSuffix(stud))
 		}
